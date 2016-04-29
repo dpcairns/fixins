a1d54a130e0fc4ad3425f3b132f2e9779aa1e16f
dpcairns <dpcairns@gmail.com>
2016-04-29 13:24:46 -0700

    locked nonadmin from admin page3

--- src/js/components/utils/Admin.js
+++ src/js/components/utils/Admin.js
@@ -20,9 +20,13 @@ export default class Admin extends React.Component{
 	render(){
 
 
-	  if(this.props.currentUser === undefined || this.props.currentUser.username !== "admin321"){
+	  if(this.props.currentUser === undefined){
 		  this.context.router.push('index/login')
+
 		  }
+		else if(this.props.currentUser.username !== "admin321"){
+			this.context.router.push('index/login')
+		}
 
 
 	return(
