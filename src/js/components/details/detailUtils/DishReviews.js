af84ada36492d6d6cb987d858769c0a570fde25d
dpcairns <dpcairns@gmail.com>
2016-04-29 10:27:20 -0700

    hanged stars agains

--- src/js/components/details/detailUtils/DishReviews.js
+++ src/js/components/details/detailUtils/DishReviews.js
@@ -20,17 +20,17 @@ function findReviewsFilter(review){
 		let dishId = review.reviewed_dish._id
             return (
 							<tr key={review._id}>
-								<td><h5>{review.review_words} </h5></td>
+								<td><h5>{review.review_words} </h3></td>
 								<td><Link to={`/user/${userId}`} onClick={putOneUserInState.bind(this, userId)}>
 									<h5>{review.review_user.username} </h5> </Link>
 								</td>
-								<td>
+								<td><h4>
 								<StarRatingComponent
 																	name="rate2"
 																	editing={false}
 																	starCount={5}
 																	value={review.review_stars}
-															/>
+															/></h4>
  								</td>
 								<td><h6>{review.review_date}</h6></td>
 							</tr>
